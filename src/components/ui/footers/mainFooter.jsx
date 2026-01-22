dc10e30d35ca8783c52eff57a0d3a47a3698d1cf
burkayakca <burkaykhan@gmail.com>
2026-01-22 22:34:59 +0300

    Minor upadate

--- src/components/ui/footers/mainFooter.jsx
+++ src/components/ui/footers/mainFooter.jsx
@@ -1,7 +1,7 @@
 function MainFooter () {
     return (
         <div className="footer">
-            <p>© {new Date().getFullYear()} <a href="https://www.github.com/burkayakca/AiKanjiDictionaryApp"> burkayakca</a>  / Licensed under MIT <br />Stroke Animations: © 2018 <a href="https://www.hanziwriter.org">Hanzi Writer</a> </p>
+            <p>© {new Date().getFullYear()} <a href="https://www.github.com/burkayakca/AiKanjiDictionaryApp"> burkayakca</a>  / Licensed under MIT <br />Stroke Animations: © 2018 <a href="https://www.hanziwriter.org">Hanzi Writer</a> <br/> Uses Gemini API by Google</p>
         </div>
     )
 }
